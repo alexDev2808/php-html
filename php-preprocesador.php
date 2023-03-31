c277df5d2094c36a6a9305dd57fc6980a25aae62
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-30 22:29:23 -0600

    Comenzando con PHP como preprocesador de HTML

---
+++ php-preprocesador.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP como preprocesador de HTML</title>
+</head>
+<body>
+    <h1>Hola esto es HTML desde PHP.</h1>
+
+    <!-- PHP es capaz de imprimir etiquetas HTML como preprocesador de HTML-->
+    <?php echo "<p>Ahora esto se esta imprimiendo con PHP</p>"; ?>
+</body>
+</html>
