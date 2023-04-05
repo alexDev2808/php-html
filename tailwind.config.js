847943fa05cc39dcb6abaddb48e35a4b5bb3f49a
AlexDev2808 <colores.2808.8000@gmail.com>
2023-04-04 20:25:06 -0600

    Creando el maquetado del formulario, añadiendo TailwindCSS al proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./formulario-contacto/*.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
